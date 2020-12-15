d5d14911f253be19589b72fa72c6dd4a121f6f97
Ade Adeola <adedotster@gmail.com>
2020-12-16 00:33:07 +0100

    add query to root function

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import GetMeetings from "./GetMeetings";
+import React from "react";
+import GetUserInfo from "./GetUserInfo";
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "https://dev-clover-server.azurewebsites.net/graphql",
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <GetMeetings
+          user__id_operator="EQUAL"
+          user_id_value={4}
+          sort_createdAt="ASC"
+          sort_updatedAt="ASC"
+          first={10}
+          after="hello"
+          offset={0}
+        />
+        <GetUserInfo id="001" />
+      </div>
+    </ApolloProvider>
   );
 }
 
